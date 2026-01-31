af911317e8126141eb45d726258d978821fe95bc
PaulMed2387 <Paul.medda.1@gmail.com>
2026-01-31 10:44:59 +0100

    2ème scéance. Fin base et index

--- main.py
+++ main.py
@@ -8,12 +8,24 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 
 
+app.secret_key='Paul_mongoDB1'
+
+#connecter a mongo_db
+client = pymongo.MongoClient("mongodb+srv://Paul-Medda:Paul_mongoDB1@site-dynamique.v1oxavg.mongodb.net/?retryWrites=true&w=majority&appName=site-dynamique")
+db = client["Hexamiel"]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    Produits_data = list(db["Produits"].find({}))
+    Api_data = list(db["Apiculteur"].find({}))
+    return render_template('index.html', Produits = Produits_data, Api = Api_data)
 
 
+@app.route("/boutique")
+def boutique():
+    Produits_data = list(db["Produits"].find({}))
+    return render_template('front/boutique.html', Produits = Produits_data)
 
 
 app.run(host='0.0.0.0', port=81)
