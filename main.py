0d49a3f63b8bbd27f5a50b958b80b258a22df433
PaulMed2387 <Paul.medda.1@gmail.com>
2026-02-07 10:48:56 +0100

    3ème scéance syncro avec bdd v2

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 from flask import Flask, render_template
-
-import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 
-from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
+mongo = os.getenv('MONGO_URI')
+client = MongoClient(mongo)
+db = client.get_database("Hexamiel")
 
-app.secret_key='Paul_mongoDB1'
-
-#connecter a mongo_db
-client = pymongo.MongoClient("mongodb+srv://Paul-Medda:Paul_mongoDB1@site-dynamique.v1oxavg.mongodb.net/?retryWrites=true&w=majority&appName=site-dynamique")
-db = client["Hexamiel"]
 
 
 @app.route('/')
 def index():
     Produits_data = list(db["Produits"].find({}))
     Api_data = list(db["Apiculteur"].find({}))
-    return render_template('index.html', Produits = Produits_data, Api = Api_data)
+    return render_template('index.html', Produits = Produits_data, Apiculteurs = Api_data)
 
 
 @app.route("/boutique")
 def boutique():
     Produits_data = list(db["Produits"].find({}))
     return render_template('front/boutique.html', Produits = Produits_data)
+
+@app.route("/page_apiculteur")
+def page_apiculteur():
+    Api_data = list(db["Apiculteur"].find({}))
+    return render_template('front/page_apiculteur.html', Api = Api_data)
 
 
 app.run(host='0.0.0.0', port=81)
