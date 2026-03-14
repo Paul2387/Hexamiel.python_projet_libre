9e0fb5820b573fd0a9459b0740e2ac6eb96c71cc
PaulMed2387 <Paul.medda.1@gmail.com>
2026-03-14 10:42:27 +0100

    4ème séance. Création des template création et connexion client

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, url_for
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import bcrypt
+
+
 
 load_dotenv()
 
@@ -10,7 +13,7 @@ app = Flask(__name__)
 mongo = os.getenv('MONGO_URI')
 client = MongoClient(mongo)
 db = client.get_database("Hexamiel")
-
+app.secret_key = os.urandom(24)
 
 
 @app.route('/')
@@ -29,6 +32,60 @@ def boutique():
 def page_apiculteur():
     Api_data = list(db["Apiculteur"].find({}))
     return render_template('front/page_apiculteur.html', Api = Api_data)
+
+
+@app.route('/connexion_client' , methods=['POST', 'GET'])
+def connexion_client(): 
+    if request.method == "GET":
+        return render_template("front/connexion_client.html")
+    
+
+    db_Client = db["Client"]
+    Client = db_Client.find_one({"nom" : request.form["Nom"]})
+    if Client:
+        if bcrypt.checkpw(request.form["mdp"].encode("utf-8"), Client["mdp"]):
+            session['role'] = Client['role']
+            session['Client'] = Client["nom"]
+        else:
+            return render_template('connexion_client.html',erreur = "les mots de passe doivent etre identiques")
+        return render_template('front/connexion_client.html',erreur = "les mots de passe doivent etre identiques")
+    else:
+        return render_template("front/connexion_client.html")   
+
+
+
+
+
+@app.route('/creation_client', methods=['POST','GET'])
+def creation_client():
+
+    if request.method == "POST":
+        db_Client = db["CLient"]
+        if (db_Client.find_one({"nom" : request.form["Nom"]})):
+            return render_template("creation_client.html", erreur = "ce nom est deja utilisé")
+        else :
+            if (request.form["mdp"] == request.form["confirme_mdp"]):
+            
+
+                nom= request.form["Nom"]
+                mdp= request.form["mdp"]
+                contact= request.form["contact"]
+
+                mdp_crypte = mdp.encode("utf-8")
+                salt= bcrypt.gensalt()
+                mdp_hash = bcrypt.hashpw(mdp_crypte, salt)
+
+                nouvel_utilisateur = {
+                    "nom" : nom,
+                    "mdp" : mdp_hash,
+                    "contact" : contact
+
+                }
+    
+    db["CLient"].insert_one(nouvel_utilisateur)
+
+    return redirect
+
 
 
 app.run(host='0.0.0.0', port=81)
